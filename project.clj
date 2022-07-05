ab2d903eac285bb682df534dc2d92d64a2277650
Corin Chaplin <corin@kroo.com>
2022-07-05 14:17:12 +0100

    Add lein release tasks

--- project.clj
+++ project.clj
@@ -9,6 +9,24 @@
                  [b-social/jason "0.1.5"]
                  [com.github.tomakehurst/wiremock "2.24.1"]
                  [org.clojure/tools.logging "1.2.4"]]
+  :plugins [[lein-eftest "0.5.3"]
+            [lein-changelog "0.3.2"]
+            [lein-shell "0.5.0"]
+            [lein-codox "0.10.7"]]
   :deploy-repositories
   {"releases" {:url "https://repo.clojars.org" :creds :gpg}}
-  :repl-options {:init-ns wiremock-wrapper})
+  :repl-options {:init-ns wiremock-wrapper}
+  :release-tasks
+  [["shell" "git" "diff" "--exit-code"]
+   ["change" "version" "leiningen.release/bump-version" "release"]
+   ["codox"]
+   ["changelog" "release"]
+   ["shell" "sed" "-E" "-i" "" "s/\"[0-9]+\\.[0-9]+\\.[0-9]+\"/\"${:version}\"/g" "README.md"]
+   ["shell" "git" "add" "."]
+   ["vcs" "commit"]
+   ["vcs" "tag"]
+   ["deploy"]
+   ["change" "version" "leiningen.release/bump-version"]
+   ["vcs" "commit"]
+   ["vcs" "tag"]
+   ["vcs" "push"]])
