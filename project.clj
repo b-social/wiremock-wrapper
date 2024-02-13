b706e2b180cc0e828b4eb1d4d93af5a26d864b2d
Robert Shilcof <robert.shilcof@gmail.com>
2024-02-13 10:31:32 +0000

    Version 0.2.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b-social/wiremock-wrapper "0.2.5"
+(defproject b-social/wiremock-wrapper "0.2.6-SNAPSHOT"
   :description "A clojure wrapper library around Java wiremock library"
   :url "https://github.com/b-social/wiremock-wrapper"
   :license {:name "The MIT License"
